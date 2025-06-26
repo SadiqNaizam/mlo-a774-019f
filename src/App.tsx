0f0abfbe52e700a29b325fbddcd70bc5ead3688a
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 19:24:02 +0530

    Fixed build errors: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "@/pages/Dashboard";
+import { Toaster } from "@/components/ui/sonner";
 
-
-import CheckoutPage from "./pages/CheckoutPage";
-import HomePage from "./pages/HomePage";
-import RestaurantDetailPage from "./pages/RestaurantDetailPage";
-import RestaurantListingPage from "./pages/RestaurantListingPage";
-import UserProfilePage from "./pages/UserProfilePage";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-
-const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
-
-
-          <Route path="/" element={<HomePage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/restaurant-detail" element={<RestaurantDetailPage />} />
-          <Route path="/restaurant-listing" element={<RestaurantListingPage />} />
-          <Route path="/user-profile" element={<UserProfilePage />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
-
-
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
-);
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      <Toaster />
+    </>
+  );
+}
 
 export default App;
